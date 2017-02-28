83ce8f41583f7216997541d83afe8f497c21f47e
inbalben <inbalkivity@gmail.com>
2017-02-28 19:37:49 +0200

    fix branch view

--- resources/views/branch.blade.php
+++ resources/views/branch.blade.php
@@ -36,7 +36,7 @@
             <div class="content_container">
                 
                 
-                 <form method="post" action="/branch">
+                <form method="post" action="/branch">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <table>
                     <tr>
@@ -65,7 +65,7 @@
                     </tr>
 
                     <tr>
-                        <td><button class="button_small" type="submit"><a href="#">Save</a></button></td>
+                        <td><button class="button_small" type="submit"><a>Save</a></button></td>
                     </tr>
 
                 </table>
